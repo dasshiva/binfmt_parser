e3506295e5dc352d92d4940e9f617070103baf47
Shivashish Das <dasshivashish11@gmail.com>
2025-03-14 12:43:17 +0530

    add lexer foundation

---
+++ lexer.c
@@ -0,0 +1,141 @@
+#include <membuf.h>
+#include "lexer.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Lexer {
+	uint32_t line;
+	uint32_t col;
+	MemBuf*  buf;
+};
+
+struct Token* next(struct Lexer* lex) {
+	if (!lex)
+		return NULL;
+	// TODO: Implement lexer
+	struct Token* ret = NULL;
+	return ret;
+}
+
+struct Lexer* newLexer(MemBuf* buf) {
+	if (!buf)
+		return NULL;
+
+	struct Lexer* ret = malloc(sizeof(struct Lexer));
+	if (!ret)
+		return NULL;
+
+	ret->buf = buf;
+	return ret;
+}
+
+void destroyLexer(struct Lexer* lexer) {
+	if (!lexer)
+		return;
+	mclose(lexer->buf);
+	free(lexer);
+}
+
+void dumpToken(struct Token* token) {
+	printf("{ Location = %u:%u ", token->line, token->col);
+	switch (token->type) {
+		case OPEN_BRACKET:
+		case CLOSE_BRACKET:
+		case COLON:
+		case COMMA:
+		case SEMI_COLON:
+		case PAR_OPEN:
+		case PAR_CLOSE:
+		case MINUS: {
+			printf("Token =\'%c\' ", (char)token->type);
+			break;
+		}
+
+		case TYPE_I8: {
+			printf(" Type = \"i8\" ");
+			break;	
+		}
+			      
+		case TYPE_I16: {
+			printf(" Type = \"i16\" ");
+			break;	
+		}
+
+		case TYPE_I32: {
+			printf(" Type = \"i32\" ");
+			break;	
+		}
+
+		case TYPE_I64: {
+			printf(" Type = \"i64\" ");
+			break;	
+		}
+
+		case TYPE_ILEB128: {
+			printf(" Type = \"ileb128\" ");
+			break;	
+		}
+
+		case TYPE_U8: {
+			printf(" Type = \"u8\" ");
+			break;	
+		}
+
+		case TYPE_U16: {
+			printf(" Type = \"u16\" ");
+			break;
+		}
+
+		case TYPE_U32: {
+			printf(" Type = \"u32\" ");
+			break;	
+		}
+
+		case TYPE_U64: {
+			printf(" Type = \"u64\" ");
+			break;	
+		}
+
+		case TYPE_ULEB128: {
+			printf(" Type = \"uleb128\" ");
+			break;	
+		}
+
+		case TYPE_VEC: {
+			printf(" Type = \"vec\" ");
+			break;	
+		}
+
+		case TYPE_SKIP: {
+			printf(" Type = \"skip\" ");
+			break;	
+		}
+
+		case TYPE_DECL: {
+			printf(" Declaration = \"type\" ");
+			break;	
+		}
+
+
+		case TYPE_CONSTANT: {
+			printf(" Constant = %lu ", token->u64);
+			break;
+		}
+
+		case TYPE_NAME: {
+			printf(" Name = %s ", token->name);
+			break;
+		}
+
+		case NEWLINE:
+		case END_OF_FILE: { break; }
+		default: {
+			printf("Unrecognised token type = %u", token->type);
+			break;
+		}
+		
+	};
+
+	printf("}\n");
+}
+
---
+++ lexer.h
@@ -0,0 +1,60 @@
+#ifndef __LEXER_H__
+#define __LEXER_H__
+
+#include <stdint.h>
+// All the tokentypes returned by the lexer
+// Notice that carriage return, tabs and spaces do not have corresponding
+// enum types. This is because the lexer simply skips over such tokens
+// pretending that they do not exist.
+// However new lines are recognised because they are used to find where
+// single line comments end
+
+enum TokenType {
+	OPEN_BRACKET  = '<', // value = 60
+	CLOSE_BRACKET = '>', // value = 63
+	TYPE_U8       =  0,  // "u8"
+	TYPE_U16,            // "u16"
+	TYPE_U32,            // "u32"
+	TYPE_U64,            // "u64"
+	TYPE_ULEB128,        // "uleb128"
+	TYPE_I8,             // "i8"
+	TYPE_I16,            // "i16"
+	TYPE_I32,            // "i32"
+	TYPE_I64,            // "i64"
+	TYPE_ILEB128,        // "ileb128"
+	TYPE_VEC,            // "vec"
+	TYPE_SKIP,           // "skip"
+	TYPE_DECL,           // "type"
+	TYPE_NAME,           // Name of a variable declared in a type(), skip, vec 
+			     // or generic variable declaration. For ex in 
+			     // "<example:u64>", this type represents the string 
+			     // "example"
+	TYPE_CONSTANT,       // A numeric constant such as 0xCAFEBABE
+			     // always treated as 64-bit unsigned integers
+	COLON    = ':',      // value = 58
+	COMMA    = ',',      // value = 44
+	SEMI_COLON = ';',    // value = 59
+	NEWLINE  = 126,      // '\n'
+	PAR_OPEN = '(',      // value = 40
+	PAR_CLOSE = ')',     // value = 41
+	MINUS    = '-',      // value = 45
+	END_OF_FILE = 127    // Returned when the lexer reaches the end of file
+};
+
+typedef struct Token {
+	union {
+		char*    name;
+	    	uint64_t u64;	
+	};
+
+	enum TokenType type;
+	uint32_t  line, col;
+} Token;
+
+struct Lexer;
+struct Lexer* newLexer();
+struct Token* next(struct Lexer* lexer);
+void dumpToken(struct Token* token);
+void destroyLexer(struct Lexer* lexer);
+
+#endif
